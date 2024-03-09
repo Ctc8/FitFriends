262b1f39a1f1fab707d69b831d4132c85dd647b3
hubbbbbbbbb <hubyu9234@gmail.com>
2024-03-08 23:49:09 -0800

    Initialized firebase for our project

---
+++ @latest/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCmKqGMJBYXruNvywPlBQvVrlGDiuuAOM",
+  authDomain: "fit-friendz.firebaseapp.com",
+  projectId: "fit-friendz",
+  storageBucket: "fit-friendz.appspot.com",
+  messagingSenderId: "677179856126",
+  appId: "1:677179856126:web:b6a35a2f10f2308edcc1af",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
