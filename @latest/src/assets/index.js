04993d2877713de1fe4dbcd5307116e06e4c3db5
angela-ling <114694052+angela-ling@users.noreply.github.com>
2024-03-09 17:11:58 -0800

    Created homepage

---
+++ @latest/src/assets/index.js
@@ -0,0 +1,9 @@
+// import Logo from "./logo.png";
+// import StreakIcon from "./streak-icon.png";
+
+const IMAGES = {
+    logo: require("./logo.png"),
+    streakIcon: require ("./streak-icon.png")
+}
+
+export default ICONS;
