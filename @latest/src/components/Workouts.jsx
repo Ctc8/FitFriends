df37c385bb9fb494b3739a48933c37f11c06bbdd
Collin C <97801841+Ctc8@users.noreply.github.com>
2024-03-09 19:46:40 -0800

    delete feature

--- @latest/src/components/Workouts.jsx
+++ @latest/src/components/Workouts.jsx
@@ -1,71 +1,91 @@
-import React from "react";
-import { Grid, TextField, Box, Button } from "@mui/material";
+import React from "react"
+import { Grid, TextField, Box, Button } from "@mui/material"
+import {useEffect} from "react"
 
-const Workout = ({ onChange }) => {
-  const handleChange = (key) => (event) => {
-    onChange(key, event.target.value);
-  };
+import DeleteIcon from "@mui/icons-material/Delete"
 
-  return (
-    <>
-      <div style={{ marginTop: "5px" }}>
-        <Box
-          component="section"
-          sx={{ bgcolor: "grey", p: 2, border: '2px solid blue', borderRadius: 2 }}
-        >
-          <Grid container spacing={2}>
-            <Grid item md={2.2}>
-              <TextField
-                label="Body Part"
-                variant="filled"
-                onChange={handleChange("bodyPart")}
-                sx={{ bgcolor: "white", borderRadius: 2 }}
-              />
-            </Grid>
-            <Grid item md={2.2}>
-              <TextField
-                label="Exercise"
-                variant="filled"
-                onChange={handleChange("exercise")}
-                sx={{ bgcolor: "white", borderRadius: 2 }}
-              />
-            </Grid>
-            <Grid item md={2.2}>
-              <TextField
-                label="Sets"
-                variant="filled"
-                type="number"
-                onChange={handleChange("sets")}
-                sx={{ bgcolor: "white", borderRadius: 2 }}
-              />
-            </Grid>
-            <Grid item md={2.2}>
-              <TextField
-                label="Reps"
-                variant="filled"
-                type="number"
-                onChange={handleChange("reps")}
-                sx={{ bgcolor: "white", borderRadius: 2 }}
-              />
-            </Grid>
-            <Grid item md={2.2}>
-              <TextField
-                fullWidth
-                label="Weight"
-                variant="filled"
-                type="number"
-                onChange={handleChange("weight")}
-                sx={{ bgcolor: "white", borderRadius: 2 }}
-              />
-            </Grid>
-            <Grid item md={1}>
-                <Button variant="outlined" sx={{ bgcolor:"whitesmoke",borderRadius: 2, width: "50px", height: "55px" }}></Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </div>
-    </>
-  );
-};
+const Workout = ({ onChange, onRemove }) => {
+	const handleChange = key => event => {
+		onChange(key, event.target.value)
+	}
 
-export default Workout;
+	return (
+		<>
+			<div style={{ marginTop: "5px" }}>
+				<Box
+					component="section"
+					sx={{
+						bgcolor: "grey",
+						p: 2,
+						border: "2px solid blue",
+						borderRadius: 2,
+					}}
+				>
+					<Grid container spacing={2}>
+						<Grid item md={2.2}>
+							<TextField
+								label="Body Part"
+								variant="filled"
+								onChange={handleChange("bodyPart")}
+								sx={{ bgcolor: "white", borderRadius: 2 }}
+							/>
+						</Grid>
+						<Grid item md={2.2}>
+							<TextField
+								label="Exercise"
+								variant="filled"
+								onChange={handleChange("exercise")}
+								sx={{ bgcolor: "white", borderRadius: 2 }}
+							/>
+						</Grid>
+						<Grid item md={2.2}>
+							<TextField
+								label="Sets"
+								variant="filled"
+								type="number"
+								onChange={handleChange("sets")}
+								sx={{ bgcolor: "white", borderRadius: 2 }}
+							/>
+						</Grid>
+						<Grid item md={2.2}>
+							<TextField
+								label="Reps"
+								variant="filled"
+								type="number"
+								onChange={handleChange("reps")}
+								sx={{ bgcolor: "white", borderRadius: 2 }}
+							/>
+						</Grid>
+						<Grid item md={2.2}>
+							<TextField
+								fullWidth
+								label="Weight"
+								variant="filled"
+								type="number"
+								onChange={handleChange("weight")}
+								sx={{ bgcolor: "white", borderRadius: 2 }}
+							/>
+						</Grid>
+
+						<Grid item md={1}>
+							<Button
+								variant="outlined"
+								sx={{
+									bgcolor: "whitesmoke",
+									borderRadius: 2,
+									width: "50px",
+									height: "55px",
+								}}
+								onClick={onRemove}
+							>
+								<DeleteIcon />
+							</Button>
+						</Grid>
+					</Grid>
+				</Box>
+			</div>
+		</>
+	)
+}
+
+export default Workout
