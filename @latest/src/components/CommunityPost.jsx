34af268400073ade9976aa36c3a193192c885cd4
Onlywar10 <121313946+Onlywar10@users.noreply.github.com>
2024-03-10 00:08:50 -0800

    midnight commit

--- @latest/src/components/CommunityPost.jsx
+++ @latest/src/components/CommunityPost.jsx
@@ -68,6 +68,7 @@ export default function CommunityPost({ selectedData, id }) {
 
     const data = await getDocs(commentsQuery);
     setComments(data.docs.map((doc) => ({ ...doc.data() })));
+    console.log(data.docs()[0].userPhoto);
   };
 
   useEffect(() => {
@@ -83,74 +84,56 @@ export default function CommunityPost({ selectedData, id }) {
 
       <div>
         {selectedData.workoutData.map((workout, index) => (
-          <div
-            key={index}
-            style={{
-              borderRadius: "20px",
-              marginTop: "10px",
-              backgroundColor: "whitesmoke",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              paddingTop: "10px",
-              paddingBottom: "10px",
-              paddingLeft: "10px",
-            }}
-          >
-            <Grid container spacing={2} alignItems="left" justifyContent="left">
-              <Grid item>
-                <Typography variant="body1" fontWeight="bold">
-                  BodyPart:
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                sx={{ marginLeft: "-10px", flexGrow: 1, marginTop: "3px" }}
-              >
-                {workout.bodyPart}
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" fontWeight="bold">
-                  Excercise:
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                sx={{ marginLeft: "-10px", flexGrow: 1, marginTop: "3px" }}
-              >
-                {workout.exercise}
-              </Grid>
-            </Grid>
-            <Box sx={{ paddingBottom: "15px" }}></Box>
-            <Grid container spacing={2} alignItems="center">
-              <Grid item md={3}>
-                <Typography variant="body1" fontWeight="bold">
-                  Sets:
-                </Typography>
-              </Grid>
-              <Grid item md={3} sx={{ flexGrow: 1 }}>
-                {workout.sets}
-              </Grid>
-              <Grid item md={3} sx={{ marginLeft: "-25px" }}>
-                <Typography variant="body1" fontWeight="bold">
-                  Reps:
-                </Typography>
-              </Grid>
-              <Grid item md={3} sx={{ marginLeft: "-10px", flexGrow: 1 }}>
-                {workout.reps}
-              </Grid>
-              <Grid item md={3}>
-                <Typography variant="body1" fontWeight="bold">
-                  Weight:
-                </Typography>
-              </Grid>
-              <Grid item md={3} sx={{ flexGrow: 1 }}>
-                {workout.weight}
-              </Grid>
-            </Grid>
-          </div>
-        ))}
+        <div key={index} style={{borderRadius:"20px", marginTop: "10px", backgroundColor: "whitesmoke", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", paddingTop: "10px", paddingBottom: "10px", paddingLeft: "10px" }}>
+         <Grid container spacing={2} alignItems="center" justifyContent="flex-start">
+  <Grid item>
+    <Typography variant="body1" fontWeight="bold">
+      BodyPart:
+    </Typography>
+  </Grid>
+  <Grid item sx={{ marginLeft: "-10px", flexGrow: 1, marginTop: "3px" }}>
+    {workout.bodyPart}
+  </Grid>
+  <Grid item>
+    <Typography variant="body1" fontWeight="bold">
+      Excercise:
+    </Typography>
+  </Grid>
+  <Grid item sx={{ flexGrow: 1, marginTop: "3px" }}>
+    {workout.exercise}
+  </Grid>
+</Grid>
+<Box sx={{ paddingBottom: "15px" }}></Box>
+<Grid container spacing={2} alignItems="center" justifyContent="flex-start">
+  <Grid item md={3}>
+    <Typography variant="body1" fontWeight="bold">
+      Sets:
+    </Typography>
+  </Grid>
+  <Grid item md={3} sx={{ flexGrow: 1 }}>
+    {workout.sets}
+  </Grid>
+  <Grid item md={3} sx={{ marginLeft: "-1px" }}>
+    <Typography variant="body1" fontWeight="bold">
+      Reps:
+    </Typography>
+  </Grid>
+  <Grid item md={3} sx={{ marginLeft: "-10px", flexGrow: 1 }}>
+    {workout.reps}
+  </Grid>
+  <Grid item md={3}>
+    <Typography variant="body1" fontWeight="bold">
+      Weight:
+    </Typography>
+  </Grid>
+  <Grid item md={3} sx={{ flexGrow: 1 }}>
+    {workout.weight}
+  </Grid>
+</Grid>
+
+
+      </div>
+      ))}
       </div>
 
       <h3 className="comments-heading">Comment</h3>
