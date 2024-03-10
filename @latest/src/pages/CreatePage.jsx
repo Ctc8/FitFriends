863c7d502bfa56d3bd291ffa0421036e0162c6e5
hubbbbbbbbb <hubyu9234@gmail.com>
2024-03-10 03:11:42 -0700

    updated

--- @latest/src/pages/CreatePage.jsx
+++ @latest/src/pages/CreatePage.jsx
@@ -8,8 +8,8 @@ import {
   Checkbox,
   FormControlLabel,
 } from "@mui/material";
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../../firebase-config.js";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db, auth } from "../../firebase-config.js";
 
 import styles from "./CreatePage.module.css";
 import logo from "../assets/logo.png";
@@ -38,12 +38,21 @@ const CreatePage = () => {
 
   const handleSubmit = async () => {
     if (Object.values(checkboxState).some((value) => value)) {
-      // workout plan json
+      const date = new Date();
+
+      console.log(workoutData);
+
       const workoutPlan = {
         name: name,
         description: description,
         workoutData: [...workoutData],
         days: checkboxState,
+        user: auth.currentUser.displayName,
+        dayCreated: date.getDay(),
+        monthCreated: date.getMonth() + 1,
+        yearCreated: date.getFullYear(),
+        timestamp: serverTimestamp(),
+        userID: auth.currentUser.uid,
       };
 
       await addDoc(postCollectionRef, workoutPlan);
