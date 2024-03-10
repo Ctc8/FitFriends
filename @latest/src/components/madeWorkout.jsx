dc92f95e912fd1ffe321ca3d486a450d1fafca91
Onlywar10 <121313946+Onlywar10@users.noreply.github.com>
2024-03-09 17:27:24 -0800

    start of you page

---
+++ @latest/src/components/madeWorkout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const madeWorkout = () => {
+  return (
+    <div>madeWorkout</div>
+  )
+}
--- @latest/src/pages/YouPage.jsx
+++ @latest/src/pages/YouPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Grid, Box} from "@mui/material";
+import logo from "../assets/logo.png"; // Import your profile icon
 
+const YourComponent = () => {
+  const [name, setName] = useState("Your Name");
+};
+  // Function to handle name change
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 const YouPage = () => {
-  return <div>YouPage</div>;
+  return (
+    <Box sx={{padding: "10px", minWidth:"1100px",}}>
+    <Grid container spacing={2} justifyContent="flex-start">
+      <Grid item style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <img src={logo} alt="" />
+        <h1>Adam Bowen</h1>
+      </Grid>
+      <Grid item>
+        <Box>
+          <h1>TEST</h1>
+        </Box>
+      </Grid>
+      <Grid item>
+        <p>Hello</p>
+      </Grid>
+    </Grid>
+  </Box>  
+  );
 };
 
 export default YouPage;
